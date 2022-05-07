d6394e55dee1381d26af6478a5aba6be4d357192
juanmagl1 <juanmabnobi1@gmail.com>
2022-05-07 19:03:25 +0200

    terminada clase jugador

--- JuegoJaca-main/src/elementos/Jugador.java
+++ JuegoJaca-main/src/elementos/Jugador.java
@@ -15,8 +15,9 @@ public class Jugador extends Element {
 		this.gemas = 0;
 		this.pociones = 0;
 	}
-	Random r=new Random();
-	
+
+	Random r = new Random();
+
 	public String getNombre() {
 		return jugadores.name();
 	}
@@ -26,17 +27,21 @@ public class Jugador extends Element {
 	}
 
 	public int getFuerzaParaLuchar() {
-		return  r.nextInt(getFuerza());
+		return r.nextInt(getFuerza());
 	}
+
 	private int getMagia() {
 		return jugadores.getMagia();
 	}
+
 	public int getMagiaParaLuchar() {
 		return r.nextInt(getMagia());
 	}
+
 	private int getVelocidad() {
 		return jugadores.getVelocidad();
 	}
+
 	public int getVelocidadParaLuchar() {
 		return r.nextInt(getVelocidad());
 	}
@@ -46,20 +51,21 @@ public class Jugador extends Element {
 	}
 
 	public void setDinero(int dinero) throws JugadorException {
-		if (dinero<0) {
+		if (dinero < 0) {
 			throw new JugadorException("Error, el dinero no puede ser menor de 0");
-		}else {
+		} else {
 			this.dinero = dinero;
-		}	
+		}
 	}
+
 	public int getPociones() {
 		return pociones;
 	}
 
 	public void setPociones(int pociones) throws JugadorException {
-		if (pociones<0) {
+		if (pociones < 0) {
 			throw new JugadorException("Las pociones no pueden ser negativas");
-		}else {
+		} else {
 			this.pociones = pociones;
 		}
 	}
@@ -69,12 +75,63 @@ public class Jugador extends Element {
 	}
 
 	public void setGemas(int gemas) throws JugadorException {
-		if (gemas<0) {
+		if (gemas < 0) {
 			throw new JugadorException("Las gemas no puedeb ser negativas");
-		}else {
+		} else {
 			this.gemas = gemas;
 		}
-		
+
+	}
+
+	public String resumen() {
+		return "Nombre: " + this.getNombre() + " Gemas: " + this.getGemas() + " Dinero: " + this.getDinero()
+				+ "Pociones: " + this.getPociones();
+	}
+
+	public PlayerType getPlayer() {
+		return jugadores;
+	}
+
+	public void encuentraDinero() {
+		this.dinero++;
+	}
+
+	public void encuentraPocion() {
+		this.pociones++;
+	}
+
+	public void encuentraGema() {
+		this.gemas++;
+	}
+
+	public int lucha(Jugador contrario) {
+		int resultado = 100000;
+		if (this.getFuerzaParaLuchar()== contrario.getFuerzaParaLuchar()) {
+			resultado=0;
+		}else if (this.getFuerzaParaLuchar()>contrario.getFuerzaParaLuchar()) {
+			if (contrario.getPociones()>0) {
+				contrario.pociones--;
+				resultado=1;
+			}else if (contrario.getDinero()>0) {
+				this.dinero+=contrario.dinero;
+				contrario.dinero=0;
+				resultado=2;
+			}else {
+				resultado=3;
+				contrario=null;
+			}
+		}else {
+			if (pociones>0) {
+				pociones--;
+			}else if (dinero>0) {
+				contrario.dinero+=this.getDinero();
+				this.dinero=0;
+				resultado=5;
+			}else {
+				resultado=6;
+			}
+		}
+		return resultado;
 	}
 	
 }
